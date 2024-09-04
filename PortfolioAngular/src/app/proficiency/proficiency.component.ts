e5c3b8a8c85c11947f172fb5882e546b534e857c
Inder-Jaiswal <inderjaiswal00@yahoo.com>
2024-09-04 22:53:14 +0530

    added the wobble effect to python progress bar

--- PortfolioAngular/src/app/proficiency/proficiency.component.ts
+++ PortfolioAngular/src/app/proficiency/proficiency.component.ts
@@ -1,10 +1,30 @@
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-proficiency',
   templateUrl: './proficiency.component.html',
-  styleUrl: './proficiency.component.css'
+  styleUrl: './proficiency.component.css',
+  animations:[
+    trigger('wobble',[
+      transition('false => true',[
+        animate('0.75s', keyframes([
+          style({transform: 'translateX(-5%)', offset:0.1}),
+          style({transform: 'translateX(5%)', offset:0.3}),
+          style({transform: 'translateX(-5%)', offset:0.5}),
+          style({transform: 'translateX(5%)', offset:0.7}),
+          style({transform: 'translateX(-5%)', offset:0.9}),
+          style({transform: 'translateX(0)', offset:1}),
+        ]))
+      ])
+    ])
+  ]
 })
 export class ProficiencyComponent {
+  protected wobbleField = false;
 
+   startwobbleField(): void
+ {
+  this.wobbleField = true;
+ }
 }
