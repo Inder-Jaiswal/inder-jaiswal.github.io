46ff1dea2c0d211fbac952e91139bc3c3fb261b8
Inder-Jaiswal <inderjaiswal00@yahoo.com>
2024-09-12 23:39:57 +0530

    change added help section

--- PortfolioAngular/src/app/reach-out/reach-out.component.ts
+++ PortfolioAngular/src/app/reach-out/reach-out.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reach-out',
   templateUrl: './reach-out.component.html',
   styleUrl: './reach-out.component.css'
 })
-export class ReachOutComponent {
+
+export class ReachOutComponent implements OnInit{
+  
+  public showSkills:boolean =false;
+
+ngOnInit(): void {
+  this.showSkills= false;
+}
 
 }
